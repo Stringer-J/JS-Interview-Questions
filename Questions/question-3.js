c339fb1b69aa5a10443ec4a5d085485536448186
Josh Stringer <joshstringer@live.com>
2024-10-15 18:00:42 -0500

    updated question 3 link to html, added data to filter to q4 so i dont have to waste time making it each time

--- Questions/question-3.js
+++ Questions/question-3.js
@@ -1,2 +1,3 @@
 // Question 3
 // Implement a function to construct a table of contents from an HTML document.
+
--- Questions/question-4.js
+++ Questions/question-4.js
@@ -1,2 +1,11 @@
 // Question 4
 // Implement a function to filter rows of data matching a specified requirement.
+
+const numbers = [
+    { number: 1, letter: 'A'},
+    { number: 2, letter: 'B'},
+    { number: 3, letter: 'C'},
+    { number: 4, letter: 'D'},
+    { number: 5, letter: 'E'},
+];
+
